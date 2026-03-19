4cfb17fcdecd758e7709f469b948e626b13195dd
Curiosity <misteryCuriosity@gmail.com>
2026-03-19 19:17:57 +0000

    Revert "release: 6.3.7"
    This reverts commit 81c9a527e294e7e7113c95088f36129f60abe074.

--- src/modusa/__init__.py
+++ src/modusa/__init__.py
@@ -1,32 +1,41 @@
 # Modusa model loader
-# Modusa models
-# Media player
-# Media recorder
-# Feature computer
-# Saver
-# Sonifier
-# Animator
-# Creator. (To create figlayouts, etc)
-# Painter
-# Setter
-# Converter
-from . import (
-    animate,
-    compute,
-    convert,
-    create,
-    load,
-    models,
-    paint,
-    play,
-    record,
-    save,
-    set,
-    sonify,
-)
+from . import load
 
-# ====================
+# Modusa models
+from . import models
+
+# Media player
+from . import play
+
+# Media recorder
+from . import record
+
+# Feature computer
+from . import compute
+
+# Saver
+from . import save
+
+# Sonifier
+from . import sonify
+
+# Animator
+from . import animate
+
+# Creator. (To create figlayouts, etc)
+from . import create
+
+# Painter
+from . import paint
+
+# Setter
+from . import set
+
+# Converter
+from . import convert
+
+#====================
 # RELEASE VERSION (MAJOR.MINOR.PATCH)
 
-__version__ = "6.3.7"  # This is dynamically used by the documentation, and pyproject.toml; Only need to change it here; rest gets taken care of.
-# ====================
+__version__ = "6.2.7" # This is dynamically used by the documentation, and pyproject.toml; Only need to change it here; rest gets taken care of.
+#====================
